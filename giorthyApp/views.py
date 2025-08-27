4daf8d657493b0cd98d54b10ecd39e9e136ece6a
Giorthy Lopez <giorthy.lopez@inacapmail.cl>
2025-08-26 22:05:29 -0400

    Modifying views --  create static file -- adding images

--- giorthyApp/views.py
+++ giorthyApp/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def inicio(request):
+    indexImages = [
+        {
+            
+        }
+    ]
     return render(request,'index.html', {'inicio': inicio})
 
 
@@ -12,19 +17,22 @@ def bici_montana(request):
             'marca':'Trek',
             'modelo':'marlin 7',
             'tipo':'montaña',
-            'precio': 55000
+            'precio': 85000,
+            'imagen':'/static/images/montana/marlin7.jpg'
         },
         {
             'marca':'Trek',
-            'modelo':'marlin 7',
+            'modelo':'marlin 8',
             'tipo':'montaña',
-            'precio': 55000
+            'precio': 1150000,
+            'imagen':'/static/images/montana/marlin8.jpg'
         },
         {
             'marca':'Trek',
-            'modelo':'marlin 7',
+            'modelo':'marlin 9',
             'tipo':'montaña',
-            'precio': 55000
+            'precio': 3490000,
+            'imagen':'/static/images/montana/marlin9.jpg'
         },
     ]
     return render(request,'bicis.html',{'bicicleta_montana':bicicleta_montana})
@@ -36,19 +44,22 @@ def bici_electrica(request):
             'marca':'Huang Queon',
             'modelo':'future200',
             'tipo':'electrica',
-            'precio': 1500000
+            'precio': 1500000,
+            'imagen':''
         },
         {
             'marca':'Huang Queon',
             'modelo':'future200',
             'tipo':'electrica',
-            'precio': 1500000
+            'precio': 1500000,
+            'imagen':''
         },
         {
             'marca':'Huang Queon',
             'modelo':'future200',
             'tipo':'electrica',
-            'precio': 1500000
+            'precio': 1500000,
+            'imagen':''
         }
     ]
     return render(request, 'bicis.html', {'bicicleta_electrica': bicicleta_electrica})
@@ -59,19 +70,22 @@ def bici_bmx(request):
             'marca':'cult',
             'modelo':'Arcade candy ',
             'tipo':'bmx',
-            'precio': 550000
+            'precio': 550000,
+            'imagen':''
         },
         {
             'marca':'Subrosa',
             'modelo':'Sunday forecaster ',
             'tipo':'bmx',
-            'precio': 995000
+            'precio': 995000,
+            'imagen':''
         },
         {
             'marca':'vans',
             'modelo':'Box rail',
             'tipo':'bmx',
-            'precio': 400000
+            'precio': 400000,
+            'imagen':''
         }
     ]
     return render(request, 'bicis.html',{'bicicleta_bmx':bicicleta_bmx})
